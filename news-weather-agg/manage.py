dbc6aadb6d4a16c894db958b27a131d2a9dbdc5b
Dsmagleby <Dsmagleby@gmail.com>
2022-09-02 15:42:33 +0200

    run startapp

--- news-weather-agg/manage.py
+++ news-weather-agg/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsAgg.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news-weather-agg.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- news-weather-agg/news-weather-agg/asgi.py
+++ news-weather-agg/news-weather-agg/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for newsAgg project.
+ASGI config for news-weather-agg project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsAgg.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news-weather-agg.settings')
 
 application = get_asgi_application()
